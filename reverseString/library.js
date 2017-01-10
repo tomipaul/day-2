2cb3b3d0fc1fd41bdc8db1dfc9d9e7299af675f6
Oluwatomi Akande <tomipaul95@gmail.com>
2017-01-10 15:10:41 +0100

    added: Implemented the reverseString function in library.js

---
+++ reverseString/library.js
@@ -0,0 +1,14 @@
+function reverseString(string){
+  if (!string) {
+  	return null;
+  }
+  r = string.split('').reverse().join('');
+  if (r === string) {
+    return true;
+  }
+  else {
+  	return r;
+  }
+}
+
+module.exports = reverseString
