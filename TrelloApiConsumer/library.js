f7fcc0daf43d62c39b6ca45c85a50ad44a144c8d
Oluwatomi Akande <tomipaul95@gmail.com>
2017-01-10 18:27:53 +0100

    Added: getBoardLists, getBoardCards, selectList, selectBoard, getSelectedBoard, createBoard, addList, addCard, getList, getCard methods

--- TrelloApiConsumer/library.js
+++ TrelloApiConsumer/library.js
@@ -24,15 +24,29 @@ Interface.on('line', (input) => {
 		case 'getList':
 			consumer.getList();
 			break;
-		case 'createList':
-			consumer.createList();
+		case 'addList':
+			consumer.addList();
 			break;
 		case 'getCard':
 			consumer.getCard();
 			break;
-		case 'createCard':
-			consumer.createCard();
+		case 'addCard':
+			consumer.addCard();
 			break;
+		case 'selectList':
+			consumer.selectList();
+			break;
+		case 'selectBoard':
+			consumer.selectBoard();
+			break;
+		case 'getSelectedBoard':
+			consumer.getSelectedBoard();
+			break;
+		case 'getBoardLists':
+			consumer.getBoardLists();
+			break;
+		case 'getBoardCards':
+			consume.getBoardCards();
 		default:
 			console.log("Invalid command!");
 			break;
@@ -64,8 +78,70 @@ class TrelloApiConsumer {
 		});
 	}
 
+	selectList() {
+		Interface.question("Enter a card id: ", (listId) => {
+			this.listId = listId;
+		})
+	}
+
+	selectBoard() {
+		Interface.question("Enter a board id: ", (boardId) => {
+			this.boardId = boardId;
+		})
+	}
+
+	getSelectedBoard() {
+		if (typeof this.boardId == 'undefined') {
+			console.log("No board selected, Please select a board");
+		}
+		else {
+			var uri = `https://api.trello.com/1/boards/${this.boardId}?lists=all&list_fields=name&key=${this.key}&token=${this.token}`;
+			request(uri, function(error, response, body) {
+				if(!error && response.statusCode==200) {
+					var boardInfo = JSON.parse(body);
+					for (var info in boardInfo) {
+						if (typeof boardInfo[info] == 'object') {
+							console.log(`${info}:`,JSON.stringify(boardInfo[info]));
+						}
+						else {
+							console.log(`${info}: ${boardInfo[info]}`);
+						}
+					}
+				}
+			});
+		}
+	}
+
+	getBoardLists() {
+		if (typeof this.boardId == 'undefined') {
+			console.log("No board selected, Please select a board with the selectBoard command");
+		}
+		else {
+			let uri = `https://api.trello.com/1/boards/${this.boardId}/lists?cards=open&card_fields=name&fields=name&key=${this.key}&token=${this.token}`
+			request(uri, function(error, response, body) {
+				if(!error && statusCode==200) {
+					console.log(body);
+				}
+			});
+		}
+	}
+
+	getBoardCards() {
+		if (typeof this.boardId=='undefined') {
+			console.log("No board selected, Please select a board with the selectBoard command")
+		}
+		else {
+			let uri = `https://api.trello.com/1/boards/${this.boardId}/cards?fields=name,idList,url&key=${this.key}&token=${this.token}`;
+			request(uri, function(error, response, body) {
+				if(!error && response.statusCode==200) {
+					console.log(body);
+				}
+			});
+		}
+	}
+
 	getBoards() {
-		var uri = `https://api.trello.com/1/members/me/boards?fields=name&key=${this.key}&token=${this.token}`;
+		let uri = `https://api.trello.com/1/members/me/boards?fields=name&key=${this.key}&token=${this.token}`;
 		request(uri, function(error, response, body) {
 			if(!error && response.statusCode == 200) {
 				for (var board of JSON.parse(body)) {
@@ -79,7 +155,7 @@ class TrelloApiConsumer {
 
 	getBoard() {
 		Interface.question('Enter Board id: ', (boardId) => {
-			var uri = `https://api.trello.com/1/boards/${boardId}?lists=all&list_fields=name&key=${this.key}&token=${this.token}`;
+			let uri = `https://api.trello.com/1/boards/${boardId}?lists=all&list_fields=name&key=${this.key}&token=${this.token}`;
 			request(uri, function(error, response, body) {
 				if(!error && response.statusCode==200) {
 					var boardInfo = JSON.parse(body);
@@ -98,7 +174,7 @@ class TrelloApiConsumer {
 
 	createBoard() {
 		Interface.question("Enter Board name: ", (boardName) => {
-			var uri = `https://api.trello.com/1/boards?name=${boardName}&key=${this.key}&token=${this.token}`;
+			let uri = `https://api.trello.com/1/boards?name=${boardName}&key=${this.key}&token=${this.token}`;
 			request({url: uri, method: "POST"}, function(error, response, body) {
 				if(!error && response.statusCode==200) {
 					console.log(body);
@@ -107,11 +183,52 @@ class TrelloApiConsumer {
 			});
 		});
 	}
-	getList() {}
-	createList() {}
-	getCard() {}
-	createCard() {}
 
+	getList() {
+		Interface.question("Enter List id: ", (listId) => {
+			let uri = `https://api.trello.com/1/lists/${listId}?fields=name&cards=open&card_fields=name&key=${this.key}&token=${this.token}`;
+			request(uri, function(error, response, body) {
+				if(!error && response.statusCode==200) {
+					console.log(body);
+				}
+			});
+		});
+	}
+
+	addList() {
+		Interface.question("Enter List Name: ", (listName) => {
+			let uri = `https://api.trello.com/1/lists?name=${listName}&idBoard=${this.boardId}&key=${this.key}&token=${this.token}`;
+			request({url:uri, method:'POST'}, function(error, response, body) {
+				if(!error && response.statusCode==200) {
+					console.log(body);
+					console.log('\n'+`List ${listName} created succesfully!`);
+				}
+			});
+		});
+	}
+
+	getCard() {
+		Interface.question("Enter Card id: ", (cardId) => {
+			let uri = `https://api.trello.com/1/cards/${cardId}?fields=name,idList&member_fields=fullName&key=${this.key}&token=$(this.token}`;
+			request(uri, function(error, response, body) {
+				if(!error && response.statusCode==200) {
+					console.log(body);
+				}
+			});
+		});
+	}
+
+	addCard() {
+		Interface.question("Enter Card name: ", (cardName) => {
+			let uri = `https://api.trello.com/1/cards?name=${cardName}&idList=${this.listId}`;
+			request({url: uri, method: "POST"}, function(error, response, body) {
+				if(!error && response.statusCode==200) {
+					console.log(body);
+					console.log('\n'+`Card ${cardName} created succesfully!`);
+				}
+			});
+		});
+	}
 }
 
 let consumer = new TrelloApiConsumer('c8ace7931717ae2fb456773699e88205');
